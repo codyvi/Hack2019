7d99ebf62c3004f862f20c4ce406eac7d1e651f9
SaulBG99 <a01282698@itesm.mx>
2019-08-24 23:51:09 -0500

    camera QR recognition -avance

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, response
+from flask import Flask, render_template, request, Response
 from camera import VideoCamera
+import pyzbar.pyzbar as pyzbar
+import time
 import logging
 import sys
 
@@ -34,14 +36,19 @@ def login():
         return render_template('index.html', result=res, content_type='application/json')
 
 @app.route('/camera')
-def index():
+def camera():
     return render_template('Camera.html')
 
 def gen(camera):
     while True:
-        frame = camera.get_frame()
+        frame, image = camera.get_frame()
+        if int(round(time.time() * 1000)) % 5000 :
+            decodedObjects = pyzbar.decode(image)
+            if decodedObjects != [] :
+                for obj in decodedObjects:
+                    print(obj.data)
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/video_feed')
 def video_feed():
