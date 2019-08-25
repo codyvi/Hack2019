9bb9748604491ae2cbd836bd01e77818dc0e1811
SaulBG99 <a01282698@itesm.mx>
2019-08-24 21:16:23 -0500

    camera updates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, response
+from camera import VideoCamera
 import logging
 import sys
 
@@ -32,5 +33,21 @@ def login():
              
         return render_template('index.html', result=res, content_type='application/json')
 
+@app.route('/camera')
+def index():
+    return render_template('Camera.html')
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
             
