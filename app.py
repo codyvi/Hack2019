d7451be6f50dee9cac1b1f25e36bc211d290f6e4
codyvi <davidcantumartinez@gmail.com>
2019-08-24 20:35:35 -0500

    Alaverga

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL,MySQLdb
+import bcrypt
 import logging
 import sys
 
@@ -6,7 +8,26 @@ import db
 
 app = Flask(__name__)
 
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'residence'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    def db_query():
+        _db = db.Database()
+        if request.method == "POST":
+            sID = request.form["sID"]
+            sPass = request.form["sPass"]
+            
--- db.py
+++ db.py
@@ -7,7 +7,7 @@ class Database:
         host = "localhost"
         user = "root"
         #password = "root"
-        db = "tecuniversity"
+        db = "residence"
         port = 3306
 
         self.con = pymysql.connect( host=host, 
@@ -18,160 +18,14 @@ class Database:
                                     cursorclass=pymysql.cursors.DictCursor)
         self.cur = self.con.cursor()
 
-
-    def list_students(self):
+    def login(self, sId, sPass):
         query = '''
-	            SELECT * 
-	            FROM Students
-	            '''
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        
-        return result
-
-    def list_student(self, student_id):
-        query = '''
-                SELECT *
-                FROM Students
-                '''
-        if student_id != '':
-            query += 'WHERE sID = {}'.format(student_id)
-            
-        query+= '''
-                order by sID
-                '''
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-
-        return result
-    
-    def insert_student(self,sID, sUPRC, sName, sAddress, sPhoneNumber, sSex, sBDate, sDepartment, sMajor):
-        query = '''
-                INSERT INTO Students (sID, sUPRC, sName, sAddress, sPhoneNumber, sSex, sBDate, sDepartment, sMajor)
-                VALUES ('{}', '{}', '{}', '{}', '{}', '{}','{}','{}','{}')
-                '''.format(sID, sUPRC, sName, sAddress, sPhoneNumber, sSex, sBDate, sDepartment, sMajor)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-    def delete_student(self, student_id):
-        query = '''
-                DELETE FROM Students
-                WHERE sID = {}
-                '''.format(student_id)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-    def list_professors(self):
-        query = '''
-                SELECT *
-                FROM Professor
-                '''
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        
-        return result
-
-    def list_professor(self, professor_id):
-        query = '''
-                SELECT *
-                FROM Professor
-                '''
-        if professor_id != '':
-            query += 'WHERE pID = {}'.format(professor_id)
-        
-        query+= '''
-                order by pID
-                '''
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-            
-        return result
-
-    def insert_professor(self,pID, pUPRC, pName, pAddress, pPhoneNumber, pSex, pBDate):
-        query = '''
-                INSERT INTO Professor (pID, pUPRC, pName, pAddress, pPhoneNumber, pSex, pBDate)
-                VALUES ('{}', '{}', '{}', '{}', '{}', '{}','{}')
-                '''.format(pID, pUPRC, pName, pAddress, pPhoneNumber, pSex, pBDate)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-    def delete_professor(self, professor_id):
-        query = '''
-                DELETE FROM Professor
-                WHERE pID = {}
-                '''.format(professor_id)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-    def list_departments(self):
-        query = '''
-                SELECT *
-                FROM Department
-                '''
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-        
-        return result
-
-    def list_department(self, department_name):
-        query = "SELECT * FROM Department "
-        if department_name != '':
-            query += "WHERE dName = '{}'".format(department_name)
-        
-        query+= '''
-                order by dId
-                '''
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-            
-        return result
-
-    def insert_department(self,dId, dName, dNumber, dPhoneNumber, dOffice):
-        query = '''
-                INSERT INTO Department (dId, dName, dNumber, dPhoneNumber, dOffice)
-                VALUES ('{}', '{}', '{}', '{}', '{}')
-                '''.format(dId, dName, dNumber, dPhoneNumber, dOffice)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-    def delete_department(self, department_id):
-        query = '''
-                DELETE FROM Department
-                WHERE dId = {}
-                '''.format(department_id)
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        self.con.commit()
-
-
-    def list_coursebysid(self, student_id):
-        query = '''
-                SELECT c.cName, s.sName 
-                FROM students s, takecourse tk, course c, groupp g 
-                WHERE s.sID = tk.sID 
-                and tk.tcGroupNumber = g.gNumber 
-                and g.cNumber = c.cNumber
-                '''
-        if student_id != '':
-            query += 'AND s.sID = {}'.format(student_id)
-            
-        query+= '''
-                order by s.sID
-                '''
+            SELECT *
+            FROM usuario
+            '''
+        if sId != '':
+            query += 'WHERE sID = {}'.format(sId)
+            query += 'AND sPass = {}'.format(sPass)
 
         print('Query: {}'.format(query), file=sys.stdout)
         self.cur.execute(query)
@@ -179,54 +33,4 @@ class Database:
 
         return result
 
-    def list_profbyid(self, proffesor_id):
-        query = '''
-               SELECT p.pID, p.pName, s.hTime
-               from schedule s, professorschedule ps, professor p 
-               WHERE s.hId = ps.hId
-               AND ps.pID = p.pID  
-                '''
-        if proffesor_id != '':
-            query += 'AND p.pID = {}'.format(proffesor_id)
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-
-        return result
-
-    def list_ecoasbyid(self, proffesor_id):
-        query = '''
-             SELECT g.gEcoa,c.cNumber, c.cName, g.gNumber, g.gSemester, g.gYear 
-             FROM professor p, groupp g, course c 
-             WHERE p.pID = g.pID 
-             AND g.cNumber = c.cNumber 
-                '''
-        if proffesor_id != '':
-            query += 'AND p.pID = {}'.format(proffesor_id)
-
-        query+= '''
-                ORDER BY g.gYear, g.gSemester, c.cNumber, g.gID
-                '''
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-
-        return result
-
-    def list_history(self, proffesor_id):
-        query = '''
-                SELECT p.pID, p.pName, c.cName
-                FROM professor p, course c, groupp gp
-                WHERE gp.cNumber = c.cNumber
-                and gp.pID = p.pID
-                '''
-        if proffesor_id != '':
-            query += 'AND p.pID = {}'.format(proffesor_id)
-
-        print('Query: {}'.format(query), file=sys.stdout)
-        self.cur.execute(query)
-        result = self.cur.fetchall()
-
-        return result
+# SELECT * FROM login_details WHERE username = ? AND password = ?
